30ed619805789a67a30cf096e0e9a8873a2da905
Raghav Kattel <arg.ahv7@gmail.com>
2025-01-03 16:00:39 +0545

    nx build fix

--- src/rabbitmq/utils.ts
+++
@@ -1,5 +0,0 @@
-import { RabbitMQModuleOptions } from './types';
-
-export const getQueueByName = (queues: RabbitMQModuleOptions['queues'], queueName: string) => {
-  return queues.find(queue => queue.name === queueName);
-};
